f261559a34ce77836671baa02b0efb3cc8668b25
Rezo Joglidze <rezojoglidze7@gmail.com>
2024-08-10 10:16:24 +0400

    Initial Commit

---
+++ SwiftUIDemo/SwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemoApp.swift
+//  SwiftUIDemo
+//
+//  Created by Rezo Joglidze on 10.08.24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
